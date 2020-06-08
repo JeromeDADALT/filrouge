67a929fb631eeb371729180d81f8c55ec395ceef
Jérôme DA DALT <jerome.dadalt@lapiscine.pro>
2020-06-08 15:45:30 +0200

    responsive + menu burger

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,5 +1,7 @@
 // menu responsive
 
+//au clic sur le menu burger ou sur le bandeau,
+// je sélectionne dans le body la classe display_menuResp pour faire apparaitre ou disparaitre le menu
 $("#burger").click(function(){
     $("body").toggleClass("display_menuResp");
 });
